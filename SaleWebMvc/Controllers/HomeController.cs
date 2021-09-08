2cf707fbae35d31540f2ffa52ee6bd28730a345d
bernoudes <lucasbernoudes@gmail.com>
2021-09-08 04:58:46 -0300

    First Model-Controller-View - Department

--- SaleWebMvc/Controllers/HomeController.cs
+++ SaleWebMvc/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SaleWebMvc.Models;
+using SaleWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
