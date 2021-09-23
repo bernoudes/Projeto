1d2c01bfa508e7cb5e09656a459d0c9cf9e55f7f
bernoudes <lucasbernoudes@gmail.com>
2021-09-23 07:07:54 -0300

    Update Seller

--- SaleWebMvc/Controllers/SellersController.cs
+++ SaleWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,8 @@
 using SaleWebMvc.Services;
 using SaleWebMvc.Models;
 using SaleWebMvc.Models.ViewModels;
+using System.Collections.Generic;
+using SaleWebMvc.Services.Exceptions;
 
 namespace SaleWebMvc.Controllers
 {
@@ -76,6 +78,46 @@ namespace SaleWebMvc.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departaments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departaments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if( id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException e)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+            
         }
     }
 }
--- SaleWebMvc/Services/SellerService.cs
+++ SaleWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SaleWebMvc.Data;
 using SaleWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using SaleWebMvc.Services.Exceptions;
 
 namespace SaleWebMvc.Services
 {
@@ -36,6 +37,23 @@ namespace SaleWebMvc.Services
         {
             _context.Add(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
